9ce426a42c6199a39ab8045b31d68483ddfb7d6a
kostspielig <kostspielig@gmail.com>
2014-07-24 12:38:41 +0200

    Add correct domain to the analytics website

--- js/analytics.js
+++ js/analytics.js
@@ -4,6 +4,6 @@
 			 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-49617369-1', 'eatoutberlin.appspot.com');
+ga('create', 'UA-49617369-1', 'eatoutberlin.com');
 ga('send', 'pageview');
 
