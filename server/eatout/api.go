93ce144d564f7ebabd2bc15c116c148175042848
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2016-10-16 19:59:19 +0200

    Do not read the data again on every request

--- server/eatout/api.go
+++ server/eatout/api.go
@@ -1,23 +1,34 @@
 package eatout
 
 import (
+	"bytes"
 	"encoding/json"
 	"log"
 	"net/http"
 )
 
-func HandleApiPlaces(w http.ResponseWriter, r *http.Request) {
+var (
+	PLACES_JSON = BuildPlacesJSON()
+)
+
+func BuildPlacesJSON() []byte {
 	places, err := ReadAllPlaces()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	encoder := json.NewEncoder(w)
+	buf := bytes.Buffer{}
+	encoder := json.NewEncoder(&buf)
 	encoder.SetEscapeHTML(false)
 	encoder.SetIndent("", "    ")
-
 	err = encoder.Encode(places)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	return buf.Bytes()
+}
+
+func HandleApiPlaces(w http.ResponseWriter, r *http.Request) {
+	w.Write(PLACES_JSON)
 }
