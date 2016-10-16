43866723985d61cd71d64e44bda0404dad716887
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2016-10-16 19:08:42 +0200

    Extract place loading functions

--- server/eatout/api.go
+++ server/eatout/api.go
@@ -2,43 +2,14 @@ package eatout
 
 import (
 	"encoding/json"
-	"fmt"
-	"github.com/ghodss/yaml"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 )
 
 func HandleApiPlaces(w http.ResponseWriter, r *http.Request) {
-	dataDir, err := os.Open("data/places")
+	places, err := ReadAllPlaces()
 	if err != nil {
 		log.Fatal(err)
-	}
-	defer dataDir.Close()
-
-	contents, err := dataDir.Readdir(0)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	places := make([]Place, 0, len(contents))
-	for _, item := range contents {
-		if item.IsDir() {
-			placePath := "data/places/" + item.Name() + "/place.yaml"
-			placeYAML, err := ioutil.ReadFile(placePath)
-			if err != nil {
-				continue
-			}
-			place := Place{}
-			err = yaml.Unmarshal(placeYAML, &place)
-			if err != nil {
-				fmt.Println("Error processing:", placePath)
-				fmt.Println(err)
-				continue
-			}
-			places = append(places, place)
-		}
 	}
 
 	placesJSON, err := json.Marshal(places)
--- server/eatout/model.go
+++ server/eatout/model.go
@@ -1,5 +1,12 @@
 package eatout
 
+import (
+	"github.com/ghodss/yaml"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
 type Place struct {
 	Name        string   `json:"name"`
 	Url         string   `json:"url"`
@@ -17,4 +24,52 @@ type Place struct {
 	Images      []string `json:"images"`
 	Title       string   `json:"title"`
 	Description string   `json:"description"`
+}
+
+const (
+	PLACES_PATH = "data/places"
+)
+
+func ReadAllPlaces() ([]Place, error) {
+	dataDir, err := os.Open(PLACES_PATH)
+	if err != nil {
+		return nil, err
+	}
+	defer dataDir.Close()
+
+	contents, err := dataDir.Readdir(0)
+	if err != nil {
+		return nil, err
+	}
+
+	places := make([]Place, 0, len(contents))
+	for _, item := range contents {
+		if item.IsDir() {
+			placeName := item.Name()
+			place, err := ReadPlace(placeName)
+			if err != nil {
+				log.Println("Could not read place:", placeName)
+			} else {
+				places = append(places, place)
+			}
+		}
+	}
+
+	return places, nil
+}
+
+func ReadPlace(placeName string) (Place, error) {
+	placePath := PLACES_PATH + "/" + placeName
+	placeYAML, err := ioutil.ReadFile(placePath + "/place.yaml")
+	if err != nil {
+		return Place{}, err
+	}
+
+	place := Place{}
+	err = yaml.Unmarshal(placeYAML, &place)
+	if err != nil {
+		return Place{}, err
+	}
+
+	return place, nil
 }
